e1a1fc795f37de19e3bc1ba49778343a77f53ece
Micro <armohamadgg12@gmail.com>
2026-07-04 00:07:07 +0400

    Update SSH_Script.py

--- SSH_Script.py
+++ SSH_Script.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import os
 from subprocess import Popen,PIPE
 import hashlib
-
+# colors:
 BLUE, RED, WHITE, YELLOW, MAGENTA, GREEN, END = '\33[94m', '\033[91m', '\33[97m', '\33[93m', '\033[1;35m', '\033[1;32m', '\033[0m'
 
 class start:
